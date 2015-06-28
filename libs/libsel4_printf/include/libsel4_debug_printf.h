e06e878220fd79bfb9e6af6b90de852251e7514c
Wink Saville <wink@saville.com>
2015-06-28 16:39:20 -0700

    Rename libsel4printf to libsel4_printf

---
+++ libs/libsel4_printf/include/libsel4_debug_printf.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015, Wink Saville
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+#ifndef __LIBSEL4_DEBUG_PRINTF_H
+#define __LIBSEL4_DEBUG_PRINTF_H
+
+#ifdef NDEBUG
+
+/** NOP printf as DEBUG is not defined */
+#define libsel4_debug_printf(...) ((void)(0))
+
+#else
+
+#include <libsel4_printf.h>
+
+/**
+ * Print a formated string to a "terminal". This supports a
+ * subset of the typical libc printf:
+ *   - %% ::= prints a percent
+ *   - %d ::= prints a positive or negative long base 10
+ *   - %u ::= prints an unsigned long base 10
+ *   - %x ::= prints a unsigned long base 16
+ *   - %p ::= prints a unsigned long assuming its a pointer base 16 with 0x prepended
+ *   - %s ::= prints a string
+ *   - %llx ::= prints a unsigned long long base 16
+ */
+#define libsel4_debug_printf(...) libsel4_printf(__VA_ARGS__)
+
+#endif
+
+#endif
