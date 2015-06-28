d2684400dd049dbc2de2926d90cc0a43f40439a7
Wink Saville <wink@saville.com>
2015-06-28 16:45:01 -0700

    Add libsel4_putchar

---
+++ libs/libsel4_putchar/include/libsel4_putchar.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2015, Wink Saville
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+#ifndef __LIBSEL4_PUTCHAR_H
+#define __LIBSEL4_PUTCHAR_H
+
+/**
+ * Output ch to a "terminal"
+ */
+void libsel4_putchar(const char ch);
+
+#endif
---
+++ libs/libsel4_putchar/src/libsel4_putchar.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2015, Wink Saville
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+#include <libsel4_putchar.h>
+
+#include <sel4/arch/syscalls.h>
+
+
+/**
+ * Output ch to a "terminal"
+ */
+void libsel4_putchar(const char ch) {
+#ifdef DEBUG
+    seL4_DebugPutChar(ch);
+#else
+    // TODO: we need seL4_PutChar ????
+#endif
+}
